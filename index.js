8ff2564908b1ba42d1b3ffcf45be8a4aab8b9194
Le Doan Manh <manhledoan14112004@gmail.com>
2025-04-12 09:37:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require('dotenv').config();
+
+const database = require("./config/database")
+
+const systemConfig = require("./config/system");
+
+const route = require('./routes/client/index.route')
+const routeAdmin = require('./routes/admin/index.route')
+
+database.connect();
+
+const app = express()
+const port = process.env.PORT;
+
+app.use(methodOverride('_method'))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded())
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+// cấu hình pug
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug')
+
+//flash
+app.use(cookieParser('JKHJHSDFGHGFH'));
+app.use(session({ cookie: { maxAge: 60000 } }));
+app.use(flash());
+
+// nhúng file tĩnh
+app.use(express.static(`${__dirname}/public`))
+
+// Route
+route(app)
+routeAdmin(app)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
